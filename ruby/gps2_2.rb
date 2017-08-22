f16339c043cb8b2e33b6027b2b9d850e66705f40
Olivia Morgan <morgan.olivia@gmail.com>
2017-08-21 17:28:14 -0700

    Add pseudocode for individual methods

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,33 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create a new hash with default value of 1.
+  # iterate through the string adding values to the new hash. 
+  # print the list to the console [use print the list method.]
+  # return hash
+# output: hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# check to see if item is already in the list.
+# if it is on list, incriment item.
+# if it is not on the list,add item to list with stated quantity to the existing hash.
+# output: updated hash
+
+# Method to remove an item from the list
+# input: list, item name
+# steps:
+# delete item from list.
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: list, item name, and optional quantity
+# steps:
+# add item to list with stated quantity to the existing hash.
+# output: updated hash
+
+# Method to print a list and make it look pretty
+# input: hash
+# steps: iterate through the hash and put item and quantity on separate line.
+# output: nil
