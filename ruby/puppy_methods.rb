86a5db888c8c9c6d2de6a94ea742a7f300bfbc45
Olivia Morgan <morgan.olivia@gmail.com>
2017-08-25 15:41:23 -0700

    Iterate over data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,83 +1,73 @@
 # #/////Method Declaration/////
-
 # class Puppy
-
 #   def fetch(toy)
 #     puts "I brought back the #{toy}!"
 #     toy
 #   end
-
 #   def speak(i)
-#   	i.times {puts "Woof!"}
+#       i.times {puts "Woof!"}
 #   end
-
 #   def roll_over
-#   	puts "*rolls over*"
+#       puts "*rolls over*"
 #   end
-
 #   def dog_years(human_years)
-#   	dog_age = human_years * 7
-#   	p dog_age
+#       dog_age = human_years * 7
+#       p dog_age
 #   end
-
 #   def stay(seconds)
-#   	stay = "." * seconds
-#   	puts "stay#{stay}okay!"
+#       stay = "." * seconds
+#       puts "stay#{stay}okay!"
 #   end
-
 #   def initialize
-#   	puts "Initializing new puppy instance..."
+#       puts "Initializing new puppy instance..."
 #   end
-
 # end
-
 # #///////Driver Code//////
-
 #  fluffy = Puppy.new
 #  fluffy.fetch("ball")
 #  fluffy.speak(4)
 #  fluffy.roll_over
 #  fluffy.dog_years(2)
 #  fluffy.stay(10)
-
 #/////Method Declaration////
-
 class Red_Panda
-
-	def initialize
-		puts "Initializing new red panda instance..."
-	end
-
-	def pounce(i)
-		i.times {puts "pounce..."}
-	end
-
-	def sleeping(red_panda)
-		puts "#{red_panda} is sleeping"
-	end
-
+    def initialize(name)
+        @name = name
+        puts "Welcome, #{name}..."
+    end
+    def pounce(i)
+        i.times {puts "#{@name} pounced..."}
+    end
+    def sleepy(i)
+        sleeps = "sleepy"
+        sleeps.sub!("ee", 'ee'* i)
+        puts "#{@name} is #{sleeps}"
+    end
 end
-
 #//////Driver Code//////
+# idgy = Red_Panda.new("Idgy")
 
-counter = 0
+# idgy.sleepy(8)
+# idgy.pounce(3)
+
+#create array of 50 names
+counter = 1
+names = []
+until counter == 51
+    names << "Idgy#{counter}"
+    counter +=1
+end
+#create empty array for collection
 red_pandas = []
-
-until counter == 50
-	
-	idgy"#{counter+1}" = Red_Panda.new 
-	red_pandas[counter] = "idgy#{counter+1}"
-	counter += 1
+# for each name, make instance of Red_Panda class
+names.each do |name|
+    red_pandas << Red_Panda.new(name)
 end
 
-puts "
-These are the red pandas: 
-#{red_pandas}"
-
-red_pandas.each do |idgy|
-	idgy.sleeping(idgy)
+red_pandas.each do |red_panda|
+    red_panda.pounce(3)
+    red_panda.sleepy(4)
 end
-
 
 
 
