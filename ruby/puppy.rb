e15f7505b08ec7eecdd4cdd0472a4b0f0a9fc6cb
Olivia Morgan <morgan.olivia@gmail.com>
2017-08-25 16:05:26 -0700

    Add pseudocode for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# CLASS ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Age: varies
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Roll over
+# Stay
+# Pounce
+# Play dead
+# Fetch
